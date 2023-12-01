be3378572c80ec2b15d497bd5354efdf8ac4986d
antoine.leberre <antoine.leberre@cgi.com>
2023-12-01 19:16:31 +0100

    Day 1

---
+++ day1_part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day1_part1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("C:/Users/antoine.leberre/RiderProjects/adventOfCode_2023/day1_part1/input.txt");
+            var result = lines.Select(line => Regex.Replace(line, "[a-zA-Z]", "")).Select(number => int.Parse(number.First() + "" + number.Last())).Sum();
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ day1_part2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day1_part2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("C:/Users/antoine.leberre/RiderProjects/adventOfCode_2023/day1_part2/input.txt");
+            var number = 0;
+            var translation = new Dictionary<string, string>
+            {
+                {"one", "1"},
+                {"two", "2"},
+                {"three", "3"},
+                {"four", "4"},
+                {"five", "5"},
+                {"six", "6"},
+                {"seven", "7"},
+                {"eight", "8"},
+                {"nine", "9"}
+            };
+            foreach (var line in lines)
+            {
+                var order = new SortedDictionary<int, string>();
+                
+                Console.WriteLine(line);
+                foreach (var keyValuePair in translation.Where(dico => line.Contains(dico.Key)))
+                {
+                    foreach (var index in Regex.Matches(line, keyValuePair.Key).Cast<Match>().Select(m => m.Index)
+                                 .ToList())
+                    {
+                        order.Add(index, keyValuePair.Value);
+                    }
+
+                    
+                }
+                
+                foreach (var keyValuePair in translation.Where(dico => line.Contains(dico.Value)))
+                {
+                    foreach (var index in Regex.Matches(line, keyValuePair.Value).Cast<Match>().Select(m => m.Index)
+                                 .ToList())
+                    {
+                        order.Add(index, keyValuePair.Value);
+                    }
+                }
+                Console.WriteLine(int.Parse(string.Join("", order.Values).First() + "" + string.Join("", order.Values).Last()));
+                number += int.Parse(string.Join("", order.Values).First() + "" + string.Join("", order.Values).Last());
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
